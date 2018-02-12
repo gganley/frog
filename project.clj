9560ac703ddd6841b4637dda345c6b0c07fe13ec
gganley <gganley@student.bridgew.edu>
2018-02-12 11:55:00 -0500

    Add apache math commons dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "gganley.github.io"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :jvm-opts ["-Xmx4G"])
-
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.apache.commons/commons-math3 "3.6.1"]]
+  :main frog.main
+  :source-paths["src"])
