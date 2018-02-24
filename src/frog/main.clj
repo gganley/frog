6e8aed1ffc9b2af9a5790af875b65dd653f51df3
gganley <gganley@student.bridgew.edu>
2018-02-24 14:23:42 -0500

    Automatically pad the input to fft to nearest power of 2

--- src/frog/main.clj
+++ src/frog/main.clj
@@ -14,6 +14,19 @@
     TransformType)))
 
 (defn get-audio-buffer [file-path]
+
+(defn- log2 [val]
+  (/ (Math/log val) (Math/log 2)))
+
+(defn- pow2? [val]
+  (let [log2-val (log2 val)]
+    (== log2-val (int log2-val))))
+
+(defn- pad-to-pow2 [coll]
+  (let [big-n (count coll)
+        nearest-pow2 (first (drop-while #(> big-n %) (iterate #(* 2 %) 1N)))]
+    (take nearest-pow2 (concat coll (repeat 0)))))
+
   (with-open [in (AudioSystem/getAudioInputStream (io/file file-path))]
     (let [frame-width (.getSampleSizeInBits (.getFormat in))
           buffer (byte-array (* (/ frame-width 8) (.getFrameLength in)))
@@ -22,9 +35,13 @@
       (.. (ByteBuffer/wrap buffer) (order ByteOrder/LITTLE_ENDIAN) asShortBuffer (get short-buffer))
       short-buffer)))
 
-(defn fft [x]
-  (.transform (FastFourierTransformer. DftNormalization/STANDARD)
-              (double-array x)
+(defn fft
+  "Fourier transform on X. If X is not a power of 2 then it is padded."
+  [x]
+  (.transform (FastFourierTransformer. DftNormalization/UNITARY)
+              (if (pow2? (count x))
+                (double-array x)
+                (double-array (pad-to-pow2 x)))
               TransformType/FORWARD))
 
 (defn spectrogram-array
