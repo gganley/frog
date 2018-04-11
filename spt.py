a4598cc516fde14354449113f53c3be2d5d0b4cd
gganley <gganley@student.bridgew.edu>
2018-04-11 15:35:08 -0400

    Process STFT for frog calls

--- spt.py
+++ spt.py
@@ -1,7 +1,11 @@
-def spt(vocalization):
+from scipy import signal
+from scipy.ndimage import gaussian_filter
+
+def spt(vocalization, sampling_rate):
     # Returns a SPT
     # Each part of a SPT onsists of...
     # A starting time /t_s/
     # A stop time /t_e/
     # And frequency bin index of each of the peaks within the track /f_t/
-    pass
+    f, t, sxx = signal.spectrogram(vocalization, sampling_rate, 'hamming', 128, int(128 - (0.85 * 128)))
+
