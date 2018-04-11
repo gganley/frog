d3e0d16a3089220d44c6da069e98a88cfe373432
gganley <gganley@student.bridgew.edu>
2018-04-11 15:27:48 -0400

    Add Spectral Peak Track implementation

---
+++ spt.py
@@ -0,0 +1,7 @@
+def spt(vocalization):
+    # Returns a SPT
+    # Each part of a SPT onsists of...
+    # A starting time /t_s/
+    # A stop time /t_e/
+    # And frequency bin index of each of the peaks within the track /f_t/
+    pass
