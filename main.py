4b4c2d7566380f27ccd98adbd2a22c05faf66b8d
gganley <gganley@student.bridgew.edu>
2018-04-20 16:43:51 -0400

    Measure the length of each syllable in ms

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from scipy import signal
-import scipy.io.wavfile as wav
-from syllable import harma
+from harma import harma
 
 
 if __name__ == '__main__':
-    sample_rate, data_array = wav.read('/Users/gganley/Processed Frog/output_bf5.wav')
-    spectrogram_object = signal.spectrogram(data_array,
-                                            sample_rate,
-                                            'hamming',
-                                            nperseg=512,
-                                            noverlap=int(512 - (512 * 0.25)))
-    omega, alpha, sxx = harma(spectrogram_object, 3)
+    t = harma('/Users/gganley/Processed Frog/output.wav')
+    for x in t:
+        print(((x[2] - x[0]) / 44100) * 1000)
